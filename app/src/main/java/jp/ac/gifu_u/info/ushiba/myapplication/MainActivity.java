28055b186112756170b19828f89abb172c401a5f
牛場健太 <ms.71t.6788@s.thers.ac.jp>
2025-04-30 14:34:05 +0900

    0430 commit

--- app/src/main/java/jp/ac/gifu_u/info/ushiba/myapplication/MainActivity.java
+++ app/src/main/java/jp/ac/gifu_u/info/ushiba/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package jp.ac.gifu_u.info.ushiba.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // ボタンを取得してリスナーを設定
+        Button exitButton = findViewById(R.id.exitButton);
+        exitButton.setOnClickListener(this);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "アプリを終了しました", Toast.LENGTH_SHORT).show();
+        finish();
     }
 }
